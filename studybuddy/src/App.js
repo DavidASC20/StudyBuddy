4480121c64b263f5d13332ecef9943aca193a25a
DavidASC20 <djchong24@gmail.com>
2024-11-21 13:07:04 -0500

    adding react router and put default route as the home

--- studybuddy/src/App.js
+++ studybuddy/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'; // Assuming Home.js is in the same directory
 
 function App() {
   return (
-    <div className="App">
-      StudyBuddy!
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
