73b1b94d78dd957837d4122362c1417d958d19a6
Kirsten Szeto <szetok2@LAPTOP-N4ATPE7F>
2025-04-08 17:59:10 -0400

    populating database departments table with prefixes
    Co-authored-by: David Chong <djchong24@gmail.com>

--- backend/src/sb_db/Database/database_init.sql
+++ backend/src/sb_db/Database/database_init.sql
@@ -1,10 +1,10 @@
 -- Drop tables if they exist.
-DROP TABLE IF EXISTS tests CASCADE;
-DROP TABLE IF EXISTS courses CASCADE;
-DROP TABLE IF EXISTS departments CASCADE;
+-- DROP TABLE IF EXISTS tests CASCADE;
+-- DROP TABLE IF EXISTS courses CASCADE;
+-- DROP TABLE IF EXISTS departments CASCADE;
 
 -- Create departments table with a prefix of exactly 4 letters.
-CREATE TABLE departments (
+CREATE TABLE IF NOT EXISTS departments (
     prefix CHAR(4) NOT NULL PRIMARY KEY,
     CHECK (prefix ~ '^[A-Za-z]{4}$')
 );
@@ -12,7 +12,7 @@ CREATE TABLE departments (
 -- Create courses table with:
 --  - code: exactly 4 digits,
 --  - dept_prefix: references departments.prefix.
-CREATE TABLE courses (
+CREATE TABLE IF NOT EXISTS courses (
     code CHAR(4) NOT NULL CHECK (code ~ '^[0-9]{4}$'),
     dept_prefix CHAR(4) NOT NULL,
     PRIMARY KEY (dept_prefix, code),
@@ -30,7 +30,7 @@ CREATE TABLE courses (
 --  - year: year taken this exam
 --  - path: a TEXT field to store file paths for PDFs,
 --  - description: optional text for any additional details.
-CREATE TABLE tests (
+CREATE TABLE IF NOT EXISTS tests (
     testid SERIAL PRIMARY KEY,
     dept_prefix CHAR(4) NOT NULL,
     code CHAR(4) NOT NULL CHECK (code ~ '^[0-9]{4}$'),
@@ -42,3 +42,58 @@ CREATE TABLE tests (
     path TEXT NOT NULL,
     description TEXT
 );
+
+-- adding departments
+
+-- HASS
+INSERT INTO departments(prefix) VALUES( 'ARTS ');
+INSERT INTO departments(prefix) VALUES( 'COGS ');
+INSERT INTO departments(prefix) VALUES( 'COMM ');
+INSERT INTO departments(prefix) VALUES( 'ECON ');
+INSERT INTO departments(prefix) VALUES( 'GSAS ');
+INSERT INTO departments(prefix) VALUES( 'IHSS ');
+INSERT INTO departments(prefix) VALUES( 'INQR ');
+INSERT INTO departments(prefix) VALUES( 'LANG ');
+INSERT INTO departments(prefix) VALUES( 'LITR ');
+INSERT INTO departments(prefix) VALUES( 'PHIL ');
+INSERT INTO departments(prefix) VALUES( 'PSYC ');
+INSERT INTO departments(prefix) VALUES( 'STSO ');
+INSERT INTO departments(prefix) VALUES( 'WRIT ');
+-- Engineering
+INSERT INTO departments(prefix) VALUES( 'BMED ');
+INSERT INTO departments(prefix) VALUES( 'CHME ');
+INSERT INTO departments(prefix) VALUES( 'CIVL ');
+INSERT INTO departments(prefix) VALUES( 'ECSE ');
+INSERT INTO departments(prefix) VALUES( 'ENGR ');
+INSERT INTO departments(prefix) VALUES( 'ENVE ');
+INSERT INTO departments(prefix) VALUES( 'ESCI ');
+INSERT INTO departments(prefix) VALUES( 'ISYE ');
+INSERT INTO departments(prefix) VALUES( 'MANE ');
+INSERT INTO departments(prefix) VALUES( 'MTLE ');
+-- Architecture
+INSERT INTO departments(prefix) VALUES( 'ARCH ');
+INSERT INTO departments(prefix) VALUES( 'LGHT ');
+-- ITWS
+INSERT INTO departments(prefix) VALUES( 'ITWS ');
+-- Science
+INSERT INTO departments(prefix) VALUES( 'ASTR ');
+INSERT INTO departments(prefix) VALUES( 'BCBP ');
+INSERT INTO departments(prefix) VALUES( 'BIOL ');
+INSERT INTO departments(prefix) VALUES( 'CHEM ');
+INSERT INTO departments(prefix) VALUES( 'CSCI ');
+INSERT INTO departments(prefix) VALUES( 'ERTH ');
+INSERT INTO departments(prefix) VALUES( 'ISCI ');
+INSERT INTO departments(prefix) VALUES( 'MATH ');
+INSERT INTO departments(prefix) VALUES( 'MATP ');
+INSERT INTO departments(prefix) VALUES( 'PHYS ');
+-- Management
+INSERT INTO departments(prefix) VALUES( 'BUSN ');
+INSERT INTO departments(prefix) VALUES( 'MGMT ');
+-- Interdisciplinary and Other
+INSERT INTO departments(prefix) VALUES( 'ADMN ');
+INSERT INTO departments(prefix) VALUES( 'USAF ');
+INSERT INTO departments(prefix) VALUES( 'ASAR ');
+INSERT INTO departments(prefix) VALUES( 'USNA ');
+-- Uncategorized
+INSERT INTO departments(prefix) VALUES( 'ILEA ');
+----------------------
